e52fe35f5cf219147e662b95175cab141523e320
leonardwgk <leonardwangka@gmail.com>
2026-02-22 18:20:31 +0700

    Fix: Update image size

--- app/Http/Controllers/ResepController.php
+++ app/Http/Controllers/ResepController.php
@@ -30,7 +30,7 @@ class ResepController extends Controller
         set_time_limit(120); 
 
         $request->validate([
-            'image' => 'required|image|max:5000',
+            'image' => 'required|image|max:15000',
         ]);
 
         // ---------------------------------------------------------
